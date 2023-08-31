5a9232b8fb1f51c0b1bb3c53c90ff7536b159662
Felipe Catão do Nascimento <SNMACT145@firjan.com.br>
2023-08-31 12:23:29 -0300

    updtaw

--- js/main.js
+++ js/main.js
@@ -9,8 +9,17 @@ YeEngine.CreateBasicTags()
 let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.WhiteTheme)
-    YeEngine.AlterTheme(YeEngine.Theme.SkyTheme)
-
+    YeEngine.AlterTheme(YeEngine.Theme.SkyTheme) 
+    YeEngine.ModifyW(YeEngine.GetlementById("card"),"90vh")
+    YeEngine.ModifyH(YeEngine.GetlementById("card"),"100vh")
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400)
+
+let Update = setInterval(()=>{
+    if(YeEngine.Questions_save[0] != undefined)
+    {
+        YeEngine.ModifyContentClass(document.getElementById("card"),YeEngine.Questions_save[0])
+        clearInterval(Update)
+    }
+},600)
